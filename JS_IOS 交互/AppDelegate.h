163e4b639b8b891b229eb5906e66d9f419484deb
Liandi <56850682@qq.com>
2019-01-16 16:55:35 +0800

    Initial Commit

---
+++ JS_IOS 交互/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JS_IOS 交互
+//
+//  Created by Liandi on 2019/1/16.
+//  Copyright © 2019年 Liandi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JS_IOS 交互/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JS_IOS 交互
+//
+//  Created by Liandi on 2019/1/16.
+//  Copyright © 2019年 Liandi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
